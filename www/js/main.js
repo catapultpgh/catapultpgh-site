1bb98e790257e84ea564aed33fff17ba861b20bb
laras126 <laras126@gmail.com>
2013-02-27 13:03:01 -0500

    add new site, archive old one

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,42 @@
+$(document).ready(function() {
 
+	// Mark current menu item
+	var url = window.location.pathname;
+	$('.nav li a').removeClass('current')
+	$('.nav').find('a[href="'+url+'"]')
+					.addClass('current');
+
+
+	// Simple tabs for Community page
+	$('.tabs li a').click(function(){
+
+		// Remove active tab style from all tabs
+		$('.tabs li a').each(function(e){
+			$(this).removeClass('current');
+			e.preventDefault;
+		});
+
+		$('.tabs li a').each(function(e){
+			$(this).removeClass('current');
+			e.preventDefault;
+		});
+
+		// Add active tab style to clicked tab
+		$(this).addClass('current');
+
+
+		// Hide/show the tab's associated section
+		var tab = $(this).attr('href');
+		$('.tab-content').removeClass('current-section');
+		$('.tab-content').each(function(){
+			$(this).hide();
+		});
+
+		$('.tab-content'+tab).fadeIn();
+
+		// Remove link functionality
+		return false;
+
+	});
+
+});
