1902f4a67ad3d05ad7d01bcff40762a50269dfeb
laras126 <laras126@gmail.com>
2013-03-04 16:32:31 -0500

    moved site files to root

---
+++ agency.php
@@ -0,0 +1,66 @@
+<?php include('parts/header.php'); ?>
+
+        <header class="header animated fadeInDown">
+            <img class="logo" alt="CatapultPGH logo" />
+            <img src="img/agencyHeaderImage.jpg" alt="CatapultPGH"/>
+            <?php include('parts/nav.html'); ?>
+        </header>
+
+        <div class="agency-tagline animated fadeIn">
+            <div class="wrap clearfix">
+                <h1>Why hire a group of people who are paid to go to work when you can hire awesome coworkers that pay to use a space every day!</h1>
+            </div>
+        </div>
+
+        <div class="wrap clearfix">
+       
+            <h2>We are a creative agency.</h2>
+
+            <div class="sixcol first">
+                <p>Working with us is a bit different. Projects start with building a team that suites your needs and not the other way around. Want a WordPress website with hand illustrations, we team you with a web designer and an artist. Need a video, we've got someone who does that too. We come to work because we like working together, not because we're getting paid to, and now we're finding interesting ways to work together more often.</p>
+            </div>
+            <div class="sixcol last">
+                <p>We like helping clients sort through complex projects where we can rely on our many talents and collaborate in ways that a regular job wouldn't allow. Check out some members below and contact us if you think we're a good fit for your next project.</p>
+                <p>Want more than a freelancer but less than an agency? Hire Catapult PGH for your next design project.</p>
+            </div>
+
+        </div>
+
+        <div class="main wrap animated fadeIn clearfix">
+           
+            <section>
+                <h2>Team</h2>
+                <div class="row">
+
+                    <div class="member first clearfix">
+                        <div class="photo first">
+                            <img src="img/members/lara.jpg" alt="Lara Schenck" />
+                        </div>
+                        <div class="member-info last">
+                            <h4><a href="http://notlaura.com" target="blank">Lara Schenck</a></h4>
+                            <p>Cras mattis consectetur purus sit amet fermentum. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                        </div>
+                    </div>
+                    
+                    <div class="member last clearfix">
+                        <div class="photo first">
+                            <img src="img/members/lara.jpg" alt="Lara Schenck" />
+                        </div>
+                        <div class="member-info last">
+                            <h4><a href="http://notlaura.com" target="blank">Lara Schenck</a></h4>
+                            <p>Cras mattis consectetur purus sit amet fermentum. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+        </div> <!-- .main -->
+
+        <div class="agency-contact animated fadeIn">
+            <div class="wrap clearfix">
+                <h1>Get in touch. <a href="mailto:inbox@catapultpgh.org">Email us</a> or call on: (412) 407-3327</h1>
+            </div>
+        </div>
+ 
+<?php include('parts/footer.html'); ?>
