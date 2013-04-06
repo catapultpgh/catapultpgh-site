f98034f0d1974cc3e67699d6409181ca6f529657
Greg Nicholas <greg.nicholas@gmail.com>
2013-04-06 19:23:50 -0400

    fixed nav url

--- parts/nav.php
+++ parts/nav.php
@@ -1,7 +1,7 @@
 <!-- Will need to change these paths for production -->
 <nav class="nav">
     <ul>
-        <li><a href="/<?php echo $root; ?>/">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="community.php">Community</a></li>
         <li><a href="agency.php">Agency</a></li>
         <li><a href="http://weblog.catapultpgh.org">Weblog</a></li>
