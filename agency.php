9089c05ace09ef3eca3d984c011113cb0357ffe7
laras126 <laras126@gmail.com>
2013-04-06 14:03:46 -0400

    added some presenters, update tagline, add some bios

--- agency.php
+++ agency.php
@@ -8,13 +8,13 @@
 
         <div class="agency-tagline animated fadeIn">
             <div class="wrap clearfix">
-                <h1>Why hire a group of people who are paid to go to work when you can hire awesome coworkers that pay to use a space every day!</h1>
+                <h1>Want the personal touch of a freelancer but the skillset of an agency?</h1>
             </div>
         </div>
 
         <div class="wrap clearfix">
        
-            <h2>We are a creative agency.</h2>
+            <h2>Welcome to Catapult, a Coöperative Creative Agency.</h2>
 
             <div class="sixcol first">
                 <p>Working with us is a bit different. Projects start with building a team that suites your needs and not the other way around. Want a WordPress website with hand illustrations, we team you with a web designer and an artist. Need a video, we've got someone who does that too. We come to work because we like working together, not because we're getting paid to, and now we're finding interesting ways to work together more often.</p>
@@ -37,7 +37,7 @@
                             <img src="img/members/elliott.jpeg" alt="Elliott Williams" />
                         </div>
                         <div class="member-info last">
-                            <h4><a href="http://elliottw.io" target="blank">Elliott Williams</a></h4>
+                            <h4><a href="http://elliott.io" target="blank">Elliott Williams</a></h4>
                             <p>Elliott holds an undergraduate degree in Cognitive Science from MIT, and a Masters in Human Computer Interaction from CMU. After completing his Masters, he and his business partners launched Pink*, a Service Design firm in Pittsburgh with a emphasis on using HCI research techniques to improve not only digital interfaces, but environments and processes as well. Elliott's areas of expertise include: digitally assisted collaborative work, ethnographic research, work flow analysis, design theory, and rapid prototyping methodologies. Before coming to Catapult, Elliott did consulting work with GlaxoSmithKline to create new strategies to enter the service industry, and worked on several projects to improve the efficacy of technology use for doctors in West Africa.</p>
                         </div>
                     </div>
@@ -60,7 +60,7 @@
                         </div>
                         <div class="member-info last">
                             <h4><a href="http://lnkd.in/PhzDku" target="blank">Bill Moore</a></h4>
-                            <p>If you need some video marketing for your business, talk to Bill. He will also willingly discuss going on adventures, playing chess, flying airplanes, speed reading, space exploration, jet-pack racing, and the proper use of an Oxford comma.</p>
+                            <p>Bill Moore has worked in the film and video industry for 13 years as a director, editor, animator, producer, and writer, creating video content for television, film, and the web. He has worked on video projects for Heinz, Bayer, The Pittsburgh Penguins, Children's Hospital, Pittsburgh CLO, Seven Springs, Industrial Scientific, The Mario Lemieux Foundation, The James Harrison Foundation, and The Pittsburgh Technology Council, to name a few. Bill is a certified Final Cut Pro editor, and has a Bachelors degree in Digital Media from The Art Institute of Pittsburgh.</p>
                         </div>
                     </div>
 
@@ -71,6 +71,18 @@
                         <div class="member-info last">
                             <h4><a href="http://raquelcps.com" target="blank">Raquel Rodriguez</a></h4>
                             <p>Raquel Rodriguez is a Pittsburgh-based creative specializing in lettering, graphic design and web design. Formerly a doctoral student, librarian, and graffiti writer she employs a bit of what she learned in all those arenas in her current practice and especially enjoys collaborating with creatives, entrepreneurs, non-profits and educational organizations. She is also co-creator of the podcast <a href="http://queerandbrown.tumblr.com/" target="blank">Queer &amp; Brown in Steeltown</a>.</p>
+                        </div>
+                    </div>
+                </div> <!-- member row -->
+
+
+                <div class="member clearfix">
+                        <div class="photo first">
+                            <img src="img/members/brian.jpeg" alt="Brian Shope" />
+                        </div>
+                        <div class="member-info last">
+                            <h4><a href="http://about.me/brian.shope" target="blank">Brian Shope</a></h4>
+                            <p>Brian has been professionally using social media for the last seven years, and consulting &amp; managing online communities for the last two. Clients have included Waffles, INCaffeinated; Antioch College; Haiti H2O; Walking Thumbs; and (re)integrate, among others. In addition, his background in organizational culture and an MA in Intercultural Studies (think anthropology) on social media, spirituality, and institutional change provide a robust approach to helping organizations focus on their services &amp; clients. Brian is also a grateful husband, proud daddy of two, and an urban bike commuter. French Press powered.</p>
                         </div>
                     </div>
                 </div> <!-- member row -->
--- index.php
+++ index.php
@@ -10,8 +10,7 @@
 
         <div class="main wrap animated fadeIn clearfix">
             <section class="sixcol first">
-                <h2>What is Catapult?</h2>
-                <p>Catapult is a coworking space, a community, a full stack agency, and a small batch coffee roastery. Catapult is a lot of disparate things that somehow work very well together. Stop by sometime.</p>
+                <h2 class="h1">What is Catapult?</h2>
                 
                 <ul class="contact-info">
                     <li><strong>Catapult PGH</strong></li>
@@ -25,9 +24,7 @@
                 <!-- <p><a href="/catapultpgh/agency">Hire us.</a></p> -->
             </section>
             <section class="sixcol last">
-                <h2>Why come to Catapult?</h2>
-                <p>Chuck short loin capicola, pancetta pork pastrami tail hamburger. T-bone chuck turkey drumstick pork boudin pork chop ribeye ham hock capicola strip steak venison short ribs shankle ground round. Pastrami biltong strip steak, ribeye kielbasa beef meatloaf leberkas ham hock chicken shoulder. Sausage kielbasa t-bone turkey.</p>
-                <p><a href="">Join us.</a></p>
+                <p>Catapult is a coworking space, a community, a full stack agency, and a small batch coffee roastery. Catapult is a lot of disparate things that somehow work very well together. Stop by sometime.</p>
             </section>
             <iframe width="100%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=5139+Penn+Ave,+Pittsburgh,+PA&amp;aq=0&amp;oq=5139+Penn+Ave,+Pittsburgh&amp;sll=36.620464,-85.0877&amp;sspn=0.0279,0.039182&amp;ie=UTF8&amp;hq=&amp;hnear=5139+Penn+Ave,+Pittsburgh,+Allegheny,+Pennsylvania+15224&amp;ll=40.464869,-79.941419&amp;spn=0.006612,0.009795&amp;t=m&amp;z=14&amp;output=embed"></iframe>
         </div> <!-- .main -->
