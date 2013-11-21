e592d4110c16fedf588a19f73baa4de00aff91a6
laras126 <laras126@gmail.com>
2013-11-21 11:16:49 -0500

    change title

--- parts/header.php
+++ parts/header.php
@@ -24,7 +24,7 @@ function printPresenter($path) {
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>CatapultPGH Co-working</title>
+        <title>CatapultPGH Coworking</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
         
