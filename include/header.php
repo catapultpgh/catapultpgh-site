5ff881c4bc211f18535f2efebd84e2497d44bd25
laras126 <laras126@gmail.com>
2013-12-31 16:55:12 -0500

    restructured to one page

--- include/header.php
+++ include/header.php
@@ -1,30 +1,3 @@
-<?php 
-
- // Loop through image directory and print markup for each
-function printAll($dir) {
-    $fdir = 'img/' . $dir . '/*';
-    foreach(glob($fdir) as $img_path) {  
-        printPresenter($img_path);
-    }
-}
-
-// Print each presenter
-function printPresenter($path) {
-    $content = '<img class="presenter" src="' . $path . '" alt="Show-n-Tell Presenter" />';
-    echo $content;
-}
-
-function printMember($n, $u, $i, $j) {
-    $name = $n;
-    $url = $u;
-    $img = $i;
-    $job = $j;
-    
-    include('include/member.php');
-}
-
-?>
-
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -49,13 +22,15 @@ function printMember($n, $u, $i, $j) {
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
 
-    <body id="<?php echo $page_id; ?>">
+    <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
-        <header class="header clearfix wrap">
-            <img class="logo" src="img/text-logo.png" alt="CatapultPGH logo" />
-            <?php include('include/nav.php'); ?>
+        <header class="site-header">
+            <div class="site-title">
+                <h1 class="h1">Catapult<span class="special">PGH</span></h1>
+            </div>
+            <p class="tagline">Coworking in Lawrenceville</p>
         </header>
 
-        <div class="main wrap clearfix animated fadeIn">
+        <div class="main wrap clearfix">
