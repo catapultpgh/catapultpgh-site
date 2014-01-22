b1776257a057f8cdccdde65b1c5e61b7f285ec68
laras126 <laras126@gmail.com>
2014-01-22 11:13:54 -0500

    add some photos, jquery scroll to next section

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
 $(document).ready(function() {
 
-	// Mark current menu item
-	$('.nav li a').each(function(index) {
-        if(this.href.trim() == window.location)
-            $(this).addClass('current');
-    });
+	// // Mark current menu item
+	// $('.nav li a').each(function(index) {
+ //        if(this.href.trim() == window.location)
+ //            $(this).addClass('current');
+ //    });
 
-	// Simple tabs for Community page
-	$('.tabs li a').click(function(e){
-		e.preventDefault();
-		// Remove active tab style from all tabs
-		$('.tabs li a').each(function(e){
-			$(this).removeClass('current');
-		});
+	// // Simple tabs for Community page
+	// $('.tabs li a').click(function(e){
+	// 	e.preventDefault();
+	// 	// Remove active tab style from all tabs
+	// 	$('.tabs li a').each(function(e){
+	// 		$(this).removeClass('current');
+	// 	});
 
-		// Add active tab style to clicked tab
-		$(this).addClass('current');
+	// 	// Add active tab style to clicked tab
+	// 	$(this).addClass('current');
 
-		// Hide/show the tab's associated section
-		var tab = $(this).attr('href');
-		$('.tab-content').removeClass('current-section');
-		$('.tab-content').each(function(){
-			$(this).hide();
-		});
+	// 	// Hide/show the tab's associated section
+	// 	var tab = $(this).attr('href');
+	// 	$('.tab-content').removeClass('current-section');
+	// 	$('.tab-content').each(function(){
+	// 		$(this).hide();
+	// 	});
 
-		$('.tab-content'+tab).fadeIn();
+	// 	$('.tab-content'+tab).fadeIn();
 
-		// Remove link functionality
-		return false;
+	// 	// Remove link functionality
+	// 	return false;
+	// });
+
+	$('.nav-arrow').click(function() {
+		var next_section = $(this).parents('.content-unit').next('.content-unit');
+      		
+		$('html,body').animate({
+  			scrollTop: next_section.offset().top
+    	}, 1000);
 	});
+
+
 
 });
